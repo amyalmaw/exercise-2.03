2d07c28524a8f6da2c69e08f52b2721973b1d800
GBTC406012UR <GBTC406012UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-22 17:03:44 -0400

    acticty2.03

---
+++ src/main/java/com/example/demo/employee.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+public class employee {
+    private long id;
+    private String name;
+    private String lastname;
+    private int ssn;
+    private int dob;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getDob() {
+        return dob;
+    }
+
+    public void setDob(int dob) {
+        this.dob = dob;
+    }
+}
